6f7ed8249e3c21520d1aa3c77afb811ceeb67fd6
SergioMainJG <arce.roldan.sergio@gmail.com>
2025-10-02 11:37:33 -0600

    modified: result.pattern.ts(new generic for errors, try method modified)

--- src/utils/patterns/result.pattern.ts
+++ src/utils/patterns/result.pattern.ts
@@ -1,29 +1,37 @@
-export class Result<T> {
+export class Result<T, E extends Error = Error> {
 
-  public static Success<T>(value: T) {
-    return new Result<T>(true, value, null);
+  public static Success<T, E extends Error = Error>(
+    value: T ): Result<T, E> {
+    return new Result<T, E>(true, value, null);
   }
 
-  public static Failure<T>(error: string) {
-    return new Result<T>(false, null, error);
+  public static Failure<T, E extends Error = Error>(
+    error: E
+  ):Result<T, E> {
+    return new Result<T,E>(false, null, error);
   }
 
-  public static HardProcess<T>(
-    callback: () => T,
-    awaitedError: string
-  ) {
+  public static async try<T>(
+    callback: () => T | Promise<T>
+  ):Promise<Result<T, Error>> {
     try {
-      const result = callback();
+      const result = await callback();
       return Result.Success(result);
-    } catch (_error) {
-      return Result.Failure(awaitedError);
+    } catch (error) {
+      if (error instanceof Error) {
+        return Result.Failure(error);
+      }
+      return Result.Failure(new Error(String(error)));
     }
   }
 
   private constructor(
-    private readonly isSuccess: boolean,
-    private readonly value: T | null,
-    private readonly error: null | string,
+    private readonly _isSuccess: boolean,
+    private readonly _value: T | null,
+    private readonly _error: null | E,
   ) { }
 
+  get isSuccess(){return this._isSuccess};
+  get value(){return this._value};
+  get error(){return this._error};
 }
