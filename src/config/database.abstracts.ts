332951ee62c1b14c4d69ce33d08f533b7a19b255
KinginWordl <diazromerol561@gmail.com>
2025-10-02 13:05:47 -0500

    update database.ts database.abstracts.ts: upgrade result promise

--- src/config/database.abstracts.ts
+++ src/config/database.abstracts.ts
@@ -1,12 +1,15 @@
 import { Result } from "../utils/patterns/result.pattern.ts";
 
+type UserTemplate = {name: string, password:string};
+
 export abstract class AbsDatabase {
   abstract createDatabase(): void;
   abstract createUserTable(): void;
   abstract createMessagesTable(): void;
   abstract addForeignKey(): void;
-  abstract createUser(): Result<number | bigint>;
-  abstract getUserById(): Result<Record<string, string>>;
-  abstract deleteUser(): Result<boolean>;
-  abstract getMessagesByUserId(): Result<Array<string>>;
+  abstract createUser(user: UserTemplate): Promise<Result<number | bigint>>;
+  abstract getUserById(id:number): Promise<Result<Record<string, string>>>;
+  abstract deleteUser(id:number): Promise<Result<boolean>>;
+  abstract addMessageOfUser(content: string, userId: number): Promise<Result<number|bigint>>;
+  abstract getMessagesByUserId(id: number): Promise<Result<Array<string>>>;
 }
