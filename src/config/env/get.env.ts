c62f632f1f6f7a93d73993d6f07c0ba84661a0ad
SergioMainJG <arce.roldan.sergio@gmail.com>
2025-10-05 01:24:30 -0600

    add: get.env.ts

---
+++ src/config/env/get.env.ts
@@ -0,0 +1,37 @@
+interface DATABASE_CONFIG {
+  readonly pathDatabase: string;
+  readonly database: string;
+  readonly tableUser: string;
+  readonly tableMessage: string;
+}
+
+interface APP_CONFIG {
+  readonly port: number;
+  readonly database: DATABASE_CONFIG;
+}
+
+const PROD: boolean = Boolean(Deno.env.get("PROD"));
+
+const getENVVars = (name: string, defaultValue: string): string => {
+  const envVar = Deno.env.get(name.toUpperCase());
+  if (
+    PROD && !envVar
+  ) {
+    throw new Error(`The enviroment variable: ${name.toUpperCase()} is required to start the application`);
+  }
+  return envVar || defaultValue;
+}
+
+const DATABASE_CONFIG: DATABASE_CONFIG = {
+  pathDatabase: getENVVars("PATH_DATABASE", "/var/data/"),
+  database: getENVVars("DATABASE", "chat-rsa-database.db"),
+  tableUser: getENVVars("TABLE_USER", "Users"),
+  tableMessage: getENVVars("TABLE_MESSAGE", "Messages")
+}
+
+const GLOBAL_CONFIG: APP_CONFIG = {
+  port: Number(getENVVars("PORT", "3000")),
+  database: DATABASE_CONFIG,
+}
+
+export default GLOBAL_CONFIG;
