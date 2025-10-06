cc98ae84c60410aacf424c1621ed1da6a2254872
SergioMainJG <arce.roldan.sergio@gmail.com>
2025-10-06 01:24:32 -0600

    add: auth.controller.ts

---
+++ src/controllers/auth/auth.controller.ts
@@ -0,0 +1,38 @@
+import { CreateUserDto } from "../../dtos/create-user.dto.ts";
+import { GetUserDto } from "../../dtos/get-user.dto.ts";
+import { JsonResponse } from '../../utils/JsonResponse.ts';
+import { CustomError } from "../../utils/errors/custom-error.ts";
+import { type AuthServices } from "../../services/auth/auth.service.ts";
+
+export class AuthController {
+  constructor(
+    public readonly authService: AuthServices
+  ) { }
+
+  private handleError = (error: unknown): JsonResponse => {
+    if (error instanceof CustomError) {
+      const code = error.statusCode;
+      const message = error.message;
+      return new JsonResponse({ error: message }, { status: code });
+    }
+    return new JsonResponse({ error: `Internal Server Error` }, { status: 500 })
+  }
+
+  register = async (req: Request): Promise<void | JsonResponse> => {
+    const body = await req.json();
+    const { isSuccess, error, value } = CreateUserDto.create(body);
+    if (!isSuccess) return new JsonResponse({ error }, { status: 400 });
+    this.authService.registerUser(value as CreateUserDto)
+      .then(user => new JsonResponse(user))
+      .catch(error => this.handleError(error))
+  }
+
+  login = async (req: Request): Promise<void | JsonResponse> => {
+    const body = await req.json();
+    const { isSuccess, error, value } = GetUserDto.create(body);
+    if (!isSuccess) return new JsonResponse({ error }, { status: 400 });
+    this.authService.loginUser(value as GetUserDto)
+      .then(user => new JsonResponse(user))
+      .catch(error => this.handleError(error));
+  }
+}
