7ee4570b9835829fd1c67f2255ff251d89a5346c
SergioMainJG <arce.roldan.sergio@gmail.com>
2025-10-06 01:24:18 -0600

    add: auth.service.ts

---
+++ src/services/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { CreateUserDto } from "../../dtos/create-user.dto.ts";
+import { GetUserDto } from "../../dtos/get-user.dto.ts";
+import { CustomError } from "../../utils/errors/custom-error.ts";
+import { JWTAdapter } from "../../config/jwt/jwt.adapter.ts";
+import { CreateUserUseCase } from '../../use-cases/user/create-user.use-case.ts';
+import { GetUserUseCase } from "../../use-cases/user/get-user.use-case.ts";
+
+export class AuthServices {
+  constructor(
+    private createUserUseCase: CreateUserUseCase,
+    private getUserUseCase: GetUserUseCase,
+  ) { }
+  public async registerUser(createUserDto: CreateUserDto) {
+    const { isSuccess, error, value } = await this.createUserUseCase.execute(createUserDto);
+    if (!isSuccess) throw error;
+    const token = await JWTAdapter.generateToken({ id: value?.id });
+    if (!token) throw CustomError.internalServer(`There's an error in the cration of JWT`);
+    return {
+      user: value,
+      token: token,
+    }
+  }
+  public async loginUser(getUserDto: GetUserDto) {
+    const { isSuccess, error, value } = await this.getUserUseCase.execute(getUserDto);
+    if(!isSuccess) throw error;
+    const token = await JWTAdapter.generateToken({id: value?.id});
+    if(!token) throw CustomError.internalServer(`There's an error in the cration of JWT`);
+    return {
+      user: value,
+      token: token
+    }
+  }
+}
